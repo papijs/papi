98dcb5490a1217f273f9f1dce2ab73cf1ed6b4f8
Andrew Farinella <andrew@bigblueswimschool.com>
2018-05-23 01:38:57 -0500

    Small changes

--- src/index.js
+++ src/index.js
@@ -1,141 +1,6 @@
-import axios from 'axios'
+import MapiEndpoint from './endpoint'
 
-const MapiEndpoint = class {
-  constructor (args) {
-    const defaults = {
-      method: 'GET',
-      endpoint: '',
-      hasParams: false,
-      params: null,
-      hasBody: false,
-      requiresAuth: false
-    }
-
-    const options = {...defaults, ...args}
-
-    // There is a param included in the endpoint but the params haven't been set up, so we'll interpret the endpoint
-    // and set up the params as best we can...
-    if (options.endpoint.includes(':') && (!options.hasParams || (options.hasParams && options.params === null))) {
-      options.params = []
-      options.hasParams = true
-
-      // This regex is used to match the param pattern ':param?'
-      const regex = /:[a-zA-Z_?]+/gm
-      let m
-      while ((m = regex.exec(options.endpoint)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-          regex.lastIndex++
-        }
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-          // Remove the leading :
-          const param = {
-            slug: match.slice(1, match.length),
-            pattern: match,
-            required: true
-          }
-
-          if (param.slug.includes('?')) {
-            param.slug = param.slug.slice(0, -1)
-            param.required = false
-          }
-
-          options.params.push(param)
-        })
-      }
-    }
-
-    this.method = options.method
-    this.endpoint = options.endpoint
-    this.hasParams = options.hasParams
-    this.params = options.params
-    this.hasBody = options.hasBody
-    this.requiresAuth = options.requiresAuth
-  }
-
-  buildRequestOptions () {
-    const options = {}
-
-    if (this.requiresAuth) {
-      // Check auth...
-    }
-
-    return options
-  }
-
-  getEndpoint (data) {
-    let endpoint = this.endpoint
-    if (this.hasParams) {
-      if (this.params.length === 1) {
-        if (data && typeof data === 'string') {
-          endpoint = endpoint.replace(this.params[0].pattern, data)
-        } else if (!this.params[0].required) {
-          endpoint = endpoint.replace(this.params[0].pattern, '')
-        } else {
-          throw new Error('Parameter required')
-        }
-      } else {
-        for (const param of this.params) {
-          if (data && data[param.slug]) {
-            endpoint = endpoint.replace(param.pattern, data[param.slug])
-          } else if (!param.required) {
-            endpoint = endpoint.replace(param.pattern, '')
-          } else {
-            throw new Error('Parameter required')
-          }
-        }
-      }
-    }
-
-    return endpoint
-  }
-
-  call (data) {
-    const options = this.buildRequestOptions()
-
-    switch (this.method) {
-      case 'GET':
-        return this.get(data, options)
-
-      case 'POST':
-        return this.post(data, options)
-
-      default:
-        throw new Error('Method not supported')
-    }
-  }
-
-  get (data, options) {
-    if (typeof data === 'number') {
-      data = data.toString()
-    }
-    const endpoint = this.getEndpoint(data)
-
-    return axios.get(endpoint, options)
-  }
-
-  post (data, options) {
-    if (this.hasBody && !data) {
-      throw new Error('API call requires body')
-    }
-
-    if (this.hasBody && !data.body) {
-      data = {
-        body: data
-      }
-    }
-
-    const body = (this.hasBody) ? data.body : null
-
-    const endpoint = this.getEndpoint(data)
-
-    return axios.post(endpoint, body, options)
-  }
-}
-
-const Service = class {
+const MapiService = class {
   constructor (args) {
     const defaults = {
       base: '/',
@@ -143,7 +8,7 @@ const Service = class {
       hasHealthCheck: true,
       healthCheck: {
         method: 'GET',
-        endpoint: '/info',
+        endpoint: '/health',
         hasBody: false,
         requiresAuth: false,
         alias: 'health'
@@ -202,7 +67,7 @@ const Service = class {
       ]
     }
 
-    this.base = options.base
+    this._base = options.base
     this.endpoints = []
 
     this.registerEndpoints(options.endpoints)
@@ -215,7 +80,7 @@ const Service = class {
 
   registerEndpoints (endpoints) {
     for (const i in endpoints) {
-      let index = this.endpoints.push(new MapiEndpoint({...endpoints[i], endpoint: this.base + endpoints[i].endpoint})) - 1
+      let index = this.endpoints.push(new MapiEndpoint({...endpoints[i], endpoint: this._base + endpoints[i].endpoint})) - 1
 
       if (endpoints[i].alias && endpoints[i].alias !== '') {
         this[endpoints[i].alias] = (data) => this.endpoints[index].call(data)
@@ -243,9 +108,9 @@ const Service = class {
         service.endpoints = service.endpoints || []
         service.services = service.services || []
 
-        service.base = this.base + service.base
+        service.base = this._base + service.base
 
-        this[service.name] = new Service(service)
+        this[service.name] = new MapiService(service)
 
         if (service.methods && typeof service.methods === 'object') {
           for (const methodName in service.methods) {
@@ -265,9 +130,16 @@ const Service = class {
   }
 }
 
-const Api = class {
+const Mapi = class {
   constructor (args) {
-    this.base = args.base
+    if (!args) {
+      throw new Error('Missing API configuration.')
+    }
+
+    if (!args.base) {
+      throw new Error('Missing API Base URL.')
+    }
+    this._base = args.base
 
     if (args.services && Array.isArray(args.services)) {
       for (const service of args.services) {
@@ -277,8 +149,11 @@ const Api = class {
   }
 
   registerService (args) {
-    if (!args.name && !args.base) {
-      return false
+    if (!args) {
+      throw new Error('Missing service configuration.')
+    }
+    if (!args.name) {
+      throw new Error('Missing service name.')
     }
 
     if (!args.name) {
@@ -293,10 +168,12 @@ const Api = class {
 
     args.endpoints = args.endpoints || []
     args.services = args.services || []
-    args.base = this.base + args.base
+    args.base = this._base + args.base
 
     if (!this[args.name]) {
-      this[args.name] = new Service(args)
+      this[args.name] = new MapiService(args)
+    } else {
+      throw new Error(`Service ${args.name} is already registered.`)
     }
 
     if (args.methods && typeof args.methods === 'object') {
@@ -311,4 +188,4 @@ const Api = class {
   }
 }
 
-export default Api
+export default Mapi
