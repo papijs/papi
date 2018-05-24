e411d9de52fa6468bb61926b3dc5541ab3f2e6a8
Andrew Farinella <andrew@bigblueswimschool.com>
2018-05-23 20:47:19 -0500

    Endpoint changes
    - Store alias in endpoint object
    - Only interpret endpoint parameters if hasParams is set
    - Remove extraneous comments
    - Throw error if trying to create an endpoint with parameters but no patterns exist in the endpoint
    - Refactor how MapiEndpoint makes axios calls

--- src/lib/endpoint.js
+++ src/lib/endpoint.js
@@ -8,14 +8,15 @@ const MapiEndpoint = class {
       hasParams: false,
       params: null,
       hasBody: false,
-      requiresAuth: false
+      requiresAuth: false,
+      alias: ''
     }
 
     const options = {...defaults, ...args}
 
     // There is a param included in the endpoint but the params haven't been set up, so we'll interpret the endpoint
     // and set up the params as best we can...
-    if (options.endpoint.includes(':') && (!options.hasParams || (options.hasParams && options.params === null))) {
+    if (options.endpoint.includes(':') && options.hasParams && options.params === null) {
       options.params = []
       options.hasParams = true
 
@@ -23,14 +24,11 @@ const MapiEndpoint = class {
       const regex = /:[a-zA-Z_?]+/gm
       let m
       while ((m = regex.exec(options.endpoint)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
         if (m.index === regex.lastIndex) {
           regex.lastIndex++
         }
 
-        // The result can be accessed through the `m`-variable.
         m.forEach((match, groupIndex) => {
-          // Remove the leading :
           const param = {
             slug: match.slice(1, match.length),
             pattern: match,
@@ -47,12 +45,29 @@ const MapiEndpoint = class {
       }
     }
 
+    if (!options.endpoint.includes(':') && options.hasParams) {
+      throw new Error(`Endpoint ${options.alias} is expecting parameters but couldn't find any in the endpoint uri`)
+    }
+
+    if (options.hasParams && typeof options.params[0] === 'string') {
+      for (const i in options.params) {
+        if (options.params.hasOwnProperty(i)) {
+          options.params[i] = {
+            slug: options.params[i],
+            pattern: ':' + options.params[i],
+            required: true
+          }
+        }
+      }
+    }
+
     this.method = options.method
     this.endpoint = options.endpoint
     this.hasParams = options.hasParams
     this.params = options.params
     this.hasBody = options.hasBody
     this.requiresAuth = options.requiresAuth
+    this.alias = options.alias
   }
 
   buildRequestOptions () {
@@ -95,43 +110,46 @@ const MapiEndpoint = class {
   call (data) {
     const options = this.buildRequestOptions()
 
-    switch (this.method) {
-      case 'GET':
-        return this.get(data, options)
-
-      case 'POST':
-        return this.post(data, options)
-
-      default:
-        throw new Error('Method not supported')
-    }
-  }
-
-  get (data, options) {
     if (typeof data === 'number') {
       data = data.toString()
     }
-    const endpoint = this.getEndpoint(data)
 
-    return axios.get(endpoint, options)
-  }
-
-  post (data, options) {
     if (this.hasBody && !data) {
-      throw new Error('API call requires body')
+      throw new Error(`MapiEndpoint ${this.alias} is expecting a request body but couldn't find any.`)
     }
 
     if (this.hasBody && !data.body) {
-      data = {
-        body: data
-      }
+      data.body = data
     }
 
+    const endpoint = this.getEndpoint(data)
     const body = (this.hasBody) ? data.body : null
 
-    const endpoint = this.getEndpoint(data)
+    switch (this.method) {
+      case 'GET':
+        return axios.get(endpoint, options)
 
-    return axios.post(endpoint, body, options)
+      case 'DELETE':
+        return axios.delete(endpoint, options)
+
+      case 'HEAD':
+        return axios.head(endpoint, options)
+
+      case 'OPTIONS':
+        return axios.options(endpoint, options)
+
+      case 'POST':
+        return axios.post(endpoint, body, options)
+
+      case 'PUT':
+        return axios.put(endpoint, body, options)
+
+      case 'PATCH':
+        return axios.patch(endpoint, body, options)
+
+      default:
+        throw new Error(`Method ${this.method} not supported in MapiEndpoint ${this.alias}`)
+    }
   }
 }
 
